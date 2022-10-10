fc854bde3e83afc9ba2224df570dbdd392a77f76
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-10 19:29:09 +0530

    code

--- factorize.cpp
+++ factorize.cpp
@@ -51,7 +51,7 @@ int main(int argc, char* argv[]) {
 				continue;
 			} else if (common.size() == 1) {
 				results.push_back(common[0]);
-				cout << common[0] << endl;
+				//cout << common[0] << endl;
 				if (common[0] == (pp - '0')) {
 					cout << " ctr = " << ((((ctr > 0) && ((ctr + 1)  % l)==0))? l:((ctr + 1) % l)) << endl;
 					cout << "pi = \t" << pp - '0' << "\t\te = \t" << ee - '0' << endl;
