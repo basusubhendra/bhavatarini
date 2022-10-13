2c92de5da98f3a91627cee393afa5289f8b63521
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-13 20:25:41 +0530

    Incorporating changes from the dry run. One step closer to Mission Complete.

--- factorize.cpp
+++ factorize.cpp
@@ -8,6 +8,45 @@
 #include "zeros.hpp"
 using namespace std;
 
+bool update_hash_table(int ctr, int* hash_table) {
+	if (hash_table[ctr] == 0) {
+		hash_table[ctr] = 1;
+	} else if (hash_table[ctr] == 1) {
+		return false;
+	}	       
+	for (int i = 0; i < 8; ++i) {
+		if (hash_table[i] == 0) return false;
+	}
+	return true;
+}
+
+bool is_satisfiable(int* triplet, float& score) {
+	bool satisfiable = true;
+	if (triplet[0] == 0 || triplet[1] == 0 || triplet[2] == 0) {
+		satisfiable = false;
+	}
+	if (satisfiable) {
+		if ((triplet[0] < triplet[1] && triplet[1] > triplet[2]) || (triplet[0] > triplet[1] && triplet[1] < triplet[2])) {
+		} else {
+			satisfiable = false;
+		}
+	}
+	if (satisfiable) {
+		if (triplet[0] == triplet[2]) {
+			score = 0.5;
+		} else {
+			score = 1.0;
+		}
+	} else {
+		if (triplet[0] == triplet[1] || triplet[1] == triplet[2]) {
+			score = 0.5;
+		} else {
+			score = 1.0;
+		}
+	}
+	return satisfiable;
+}
+
 int main(int argc, char* argv[]) {
 	FILE* testcase = fopen("testcases/latest.txt","r");
 	char* num = new char[301];
@@ -16,6 +55,10 @@ int main(int argc, char* argv[]) {
 	//	char* num = strdup(argv[1]);
 	unsigned long long int l = strlen(num);
 	unsigned long long int ctr = 0;
+	vector<int*>* result = new vector<int*>();
+	int* hash_table = (int*) calloc(8, sizeof(int));
+	int* triplet = (int*) calloc(3, sizeof(int));
+	int idx = 0;
 	while (1) {
 		int left_digit = num[ctr % l] - '0';
 		int right_digit = num[(ctr + 2) % l] - '0';
@@ -50,8 +93,32 @@ int main(int argc, char* argv[]) {
 				ctr++;
 				continue;
 			} else if (common.size() == 1) {
-				cout << "ctr = " << ctr % 8 << "\t\tctr % 300 = " << (ctr + 1) % 300 << endl;
-				system("a=1;read a");
+#ifdef _DEBUG
+				cout << "ctr = " << ctr % 8 << endl;
+#endif
+				bool success = update_hash_table(ctr % 8, hash_table);
+				if (success) {
+					triplet[idx++] = ctr % 8;
+					if (idx % 3 == 0) {
+						result->push_back(triplet);
+						cout << "Triplet "<< endl;
+						for (int i = 0; i < 3; ++i) {
+							cout << triplet[i] << " , ";
+						}
+						float score = 0.0;
+						bool satisfiable = is_satisfiable(triplet, score);
+						if (satisfiable) {
+							cout << "satisfiable" << "\t\t" << score << endl;
+						} else {
+							cout << "not satisfiable" << "\t\t" << score << endl;
+						}
+						system("a=1; read a");
+						triplet = (int*) calloc(3, sizeof(int));
+						idx = 0;
+					}
+					success = false;
+	                                hash_table = (int*) calloc(8, sizeof(int));
+				}
 				common.clear();
 				ctr++;
 				continue;
@@ -59,5 +126,6 @@ int main(int argc, char* argv[]) {
 		}	
 		++ctr;
 	}
+	delete result;
 	return 0;
 }
