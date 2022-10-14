b9137bc01c93ff4ab02a65bb123ef4be2f90578e
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-14 12:20:51 +0530

    code

--- archive/create.py
+++ archive/create.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-f=open("./zeros.hpp","w")
-f.write("#ifndef _ZEROS_")
+f=open("./zero_positions.hpp","w")
+f.write("#ifndef _ZERO_POSITIONS_")
 f.write("\n")
-f.write("#define _ZEROS_")
+f.write("#define _ZERO_POSITIONS_")
 f.write("\n")
-f.write("std::vector< std::vector< std::vector< std::vector<int> > > > left_endings = {")
+f.write("std::vector< std::vector< std::vector< std::vector<int> > > > left_positions = {")
 g=open("./zeros1","r")
 lines=g.readlines()
 #left endings
@@ -12,25 +12,29 @@ for x in range(0, 7):
    f.write(" { ")
    for y in range(0, 10):
       f.write(" { ")
+      ctr = 0
       for line in lines:
           l = str(line).lstrip().rstrip()
           if l[x] == str(y):
-              f.write(str(l[x + 1]))
+              f.write(str(ctr + 1))
               f.write(",")
+          ctr = ctr + 1
       f.write(" },")
    f.write(" },")
 f.write(" };")
 f.write("\n")
-f.write("std::vector<std::vector<std::vector<std::vector<int> > > > right_endings = {")
+f.write("std::vector<std::vector<std::vector<std::vector<int> > > > right_positions = {")
 for x in range(1, 8):
    f.write(" { ")
    for y in range(0, 10):
       f.write(" { ")
+      ctr = 0
       for line in lines:
           l = str(line).lstrip().rstrip()
           if l[x] == str(y):
-              f.write(str(l[x - 1]))
+              f.write(str(ctr + 1))
               f.write(",")
+          ctr = ctr + 1
       f.write(" },")
    f.write(" },")
 f.write(" };")
