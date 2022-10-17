b475d5201788ebea4ce62426bf59062cf69c4fa4
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-17 14:16:09 +0530

    code

--- factorize.py
+++ factorize.py
@@ -5,6 +5,25 @@ from mpmath import *
 from pi import *
 from e import *
 
+def characterize(num):
+    ctr = 0
+    l = len(num)
+    triplets = []
+    while ctr % l != 0 or ctr == 0:
+         triplet = []
+         zero_counter = 0
+         for x in range(0, 3):
+             nn = num[(ctr + x) % l]
+             if nn == '0':
+                 zero_counter = zero_counter + 1
+             triplet.append(nn)
+         triplet = "".join(triplet)
+         triplets.append(triplet)
+         ctr = ctr + (3 - (zero_counter % 3))
+    return triplets
+        
 if __name__ == "__main__":
     num = str(sys.argv[1])
     print(num)
+    triplets = characterize(num)
+    print(triplets)
