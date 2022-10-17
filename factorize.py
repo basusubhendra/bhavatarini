323c25b41bbbad03ce3b41209c91cd38958cf232
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-17 14:06:43 +0530

    First Commit into Python Code, reading and printing a number.

---
+++ factorize.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+from gmpy2 import *
+from mpmath import *
+from pi import *
+from e import *
+
+if __name__ == "__main__":
+    num = str(sys.argv[1])
+    print(num)
