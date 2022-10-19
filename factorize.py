c4495003542be817ef09339667fb9ae5700f4c77
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-19 06:55:12 +0530

    1 step to code complete. Synthesis cycle coded. Over to analysis to emit the factors.

---
+++ factorize.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+from generate_output import *
+
+def characterize(num):
+    ctr = 0
+    l = len(num)
+    if l <= 3:
+        return [num]
+    triplets = []
+    while (ctr + 3) <= l:
+        triplets.append(num[ctr:(ctr + 3)])
+        ctr = ctr + 1
+    return triplets
+
+if __name__ == "__main__":
+    num = str(sys.argv[1])
+    triplets = characterize(num)
+    print(triplets)
+    multiplier = [ 8, 10, 1 ]
+    i = 0
+    ll = len(triplets)
+    solution_vector = []
+    for x in triplets[::-1]:
+        for y in range(0, multiplier[i]):
+            solution_vector.append(x)
+        i = ( i + 1 ) % 3
+    solution_vector = solution_vector[::-1]
+    lx = len(solution_vector)
+    posits = synthesize(solution_vector) 
+    factor1, factor2 = analyze(posits, solution_vector)
+    
--- generate_output.py
+++ generate_output.py
@@ -2,28 +2,32 @@
 import sys
 from pi import pi
 from e import e
-from rsa2048 import num
-#num = str(sys.argv[1])
-n=num*8
-ll = len(n)
-pp = pi[:ll]
-ee = e[:ll]
-l = len(num)
-position = 0
-posits = []
-n = num*8
-for x in list(zip(pp, n, ee)):
-    if x[0] == '0' or x[1] == '0' or x[2] == '0':
+
+def synthesize(solution_vector):
+    ll = len(solution_vector)
+    pp = pi[:ll]
+    ee = e[:ll]
+    position = 0
+    posits = []
+    for x in list(zip(pp, solution_vector, ee)):
+        if x[0] == '0' or x[1] == '0' or x[2] == '0':
+            pass
+        elif x[0] == x[2] and int(x[1]) < int(x[0]) and int(x[1]) < int(x[2]):
+            posits.append(-position)
+        elif x[0] == x[2] and int(x[1]) > int(x[0]) and int(x[1]) > int(x[2]):
+            posits.append(-position)
+        elif int(x[0]) < int(x[1]) and int(x[2]) < int(x[1]):
+            posits.append(position)
+        elif int(x[0]) > int(x[1]) and int(x[2]) > int(x[1]):
+            posits.append(position)
+        else:
+            pass
+        position = position + 1
+    return posits
+
+def analyze(posits, solution_vector):
+    ll = len(solution_vector)
+    pp = pi[:ll]
+    ee = e[:ll][::-1]
+    for x in list(zip(pp, solution_vector, ee)):
         pass
-    elif x[0] == x[2] and int(x[1]) < int(x[0]) and int(x[1]) < int(x[2]):
-        posits.append(-position)
-    elif x[0] == x[2] and int(x[1]) > int(x[0]) and int(x[1]) > int(x[2]):
-        posits.append(-position)
-    elif int(x[0]) < int(x[1]) and int(x[2]) < int(x[1]):
-        posits.append(position)
-    elif int(x[0]) > int(x[1]) and int(x[2]) > int(x[1]):
-        posits.append(position)
-    else:
-        pass
-    position = position + 1
-ee = ee[::-1]
