a9d0dc537fa69cd31ddd00b729429ae43b7cebc1
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-20 07:16:35 +0530

    code

--- factorize.py
+++ factorize.py
@@ -14,24 +14,43 @@ def characterize(num):
         ctr = ctr + 1
     return triplets
 
+def decompose(solution_vector):
+    nn = ""
+    for x in solution_vector:
+        nn = nn + x
+    return nn
+
 if __name__ == "__main__":
-    num = str(sys.argv[1])
+    num = str(sys.argv[1]).lstrip().rstrip()
     triplets = characterize(num)
-    print(triplets)
-    multiplier = [ 8, 10, 1 ]
+    multiplier = [ 10, 8 ]
     i = 0
     ll = len(triplets)
     solution_vector = []
-    for x in triplets[::-1]:
+    for x in triplets:
         for y in range(0, multiplier[i]):
             solution_vector.append(x)
-        i = ( i + 1 ) % 3
-    solution_vector = solution_vector[::-1]
+        i = ( i + 1 ) % 2
     lx = len(solution_vector)
     posits = synthesize(solution_vector) 
-    print(solution_vector)
+    ctr = 0
+    triplet1 = []
+    triplet2 = []
+    triplet3 = []
+    nn = decompose(solution_vector)
+    for x in list(zip(pi[:lx], nn, e[:lx][::-1])):
+        triplet1.append(x[0])
+        triplet2.append(x[1])
+        triplet3.append(x[2])
+        ctr = ctr + 1
+        if ctr % 3 == 0:
+            print(["".join(triplet1), "".join(triplet2), "".join(triplet3)])
+            triplet1 = []
+            triplet2 = []
+            triplet3 = []
+            ctr = 0
+    print("")
+    print("===================")
+    print("")
     print(posits)
-    print(pi[:len(solution_vector)])
-    print(e[:len(solution_vector)][::-1])
-#    factor1, factor2 = analyze(posits, solution_vector)
     
